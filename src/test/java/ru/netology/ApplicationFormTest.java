74bb33dfdbfa0007903a1bcc5afe507b2b124b95
NastiaZe <anastasiazemskova4212gmail.com>
2021-03-15 22:30:31 +0300

    commit

--- src/test/java/ru/netology/ApplicationFormTest.java
+++ src/test/java/ru/netology/ApplicationFormTest.java
@@ -1,20 +1,22 @@
 package ru.netology;
 
-import com.codeborne.selenide.SelenideElement;
+    import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-class CallbackTest {
-    @Test
-    void shouldTest() {
-        open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Настя");
-        $("[data-test-id=phone] input").setValue("+79955544");
-        $("[data-test-id=agreement]").click();
-        $("[data-test-id=submit]").click();
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+public class ApplicationFormTest {
+        @Test
+        void shouldTest() {
+            open("http://localhost:9999");
+            $("[data-test-id=name] input").setValue("Настя");
+            $("[data-test-id=phone] input").setValue("+7923665588");
+            $("[data-test-id=agreement]").click();
+            $("[data-test-id=submit]").click();
+            $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+        }
     }
-}
+
+
