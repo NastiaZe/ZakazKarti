4523a5f979eb53ea1b3ca79e58198aa5d6678a06
NastiaZe <anastasiazemskova4212gmail.com>
2021-03-15 22:23:54 +0300

    commit

--- src/test/java/ru/netology/ApplicationFormTest.java
+++ src/test/java/ru/netology/ApplicationFormTest.java
@@ -1,22 +1,20 @@
 package ru.netology;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.*;
-import static org.openqa.selenium.By.cssSelector;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-public class ApplicationFormTest {
-
+class CallbackTest {
     @Test
-    void shouldTestFormWithValidData() {
+    void shouldTest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[action]");
-        form.$(cssSelector("[type='text']")).sendKeys("Пугачёва Алла");
-        form.$(cssSelector("[type='tel']")).sendKeys("+79035648978");
-        form.$(cssSelector("[data-test-id=agreement]")).click();
-        form.$(cssSelector("[type='button']")).click();
-        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена!"));
+        $("[data-test-id=name] input").setValue("Настя");
+        $("[data-test-id=phone] input").setValue("+79955544");
+        $("[data-test-id=agreement]").click();
+        $("[data-test-id=submit]").click();
+        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
     }
 }
