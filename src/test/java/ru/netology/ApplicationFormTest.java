d99c23bb4ca23096038405c3307e230a1bfbc5b2
NastiaZe <anastasiazemskova4212gmail.com>
2021-03-15 22:47:58 +0300

    commit

--- src/test/java/ru/netology/ApplicationFormTest.java
+++ src/test/java/ru/netology/ApplicationFormTest.java
@@ -1,22 +1,60 @@
 package ru.netology;
 
-    import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class ApplicationFormTest {
-        @Test
-        void shouldTest() {
-            open("http://localhost:9999");
-            $("[data-test-id=name] input").setValue("Настя");
-            $("[data-test-id=phone] input").setValue("+7923665588");
-            $("[data-test-id=agreement]").click();
-            $("[data-test-id=submit]").click();
-            $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
-        }
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
     }
+
+    @Test
+    public void shouldReturnSuccessfullyForm() {
+
+        $(".form_theme_alfa-on-white");
+        $("[data-test-id=name] input").setValue("Пугачева Алла");
+        $("[data-test-id=phone] input").setValue("+7963221123");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    public void shouldReturnErrorIfInvalidName() {
+
+        $(".form_theme_alfa-on-white");
+        $("[data-test-id=name] input").setValue("Max");
+        $("[data-test-id=phone] input").setValue("+79875554123");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(".input_type_text .input__sub").shouldHave(exactText("Имя и Фамилия указаны неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    public void shouldReturnErrorIfInvalidTel() {
+
+        $(".form_theme_alfa-on-white");
+        $("[data-test-id=name] input").setValue("Пугачева Алла");
+        $("[data-test-id=phone] input").setValue("5874");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(".input_type_tel .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+    }
+
+    @Test
+    public void shouldReturnErrorIfFieldNameIsEmpty() {
+        $(".form_theme_alfa-on-white");
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+79874565321");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(".input_type_text .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+}
 
 
